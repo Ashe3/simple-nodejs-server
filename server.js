0ec8a5beadb60897058c02e8266304dfb282b7d8
Ashe3 <korolewsky.anton3@gmail.com>
2020-04-03 10:06:16 +0300

    public pages

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+
 const App = require('./app');
 
 const app = new App();
 
 app.use((request, response) => {
-  response.end('Hello World');
+  const { url } = request;
+  const dirname = path.join('public', url === '/' ? 'index' : url);
+  console.log(app.routes);
+
+  fs.readFile(dirname + '.html', null, (err, data) => {
+    if (err) {
+      response.writeHead(404);
+      response.write(fs.readFileSync('public/404.html'));
+    } else {
+      response.write(data);
+    }
+    response.end();
+  });
 });
 
 app.get('/hello', (request, response) => {
-  response.end('Hi bro');
+  response.end('hi bros');
   console.log(request.headers['user-agent']);
 });
 
